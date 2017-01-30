f33ced4248b74f55947330a6fc8eaecbeb414587
David Sanford <drdavid.sanford@gmail.com>
2017-01-30 14:30:27 -0800

    Updates

---
+++ common.py
@@ -0,0 +1,30 @@
+import re
+from sqlalchemy import create_engine
+
+def sql_engine():
+    fin = open("sql_credentials.txt")
+
+    username = ""
+    password = ""
+    database = ""
+    
+    for line in fin:
+        matches = re.search("(\w+) : (.+)",line)
+
+        if matches is not None:
+
+            if matches.group(1) == "username":
+                username = matches.group(2)
+
+            if matches.group(1) == "password":
+                password = matches.group(2)
+
+            if matches.group(1) == "database":
+                database = matches.group(2)
+
+    connect_param = "postgresql://{}:{}@joshuacook.me:5432/{}".format(\
+        username, password, database)
+
+    engine = create_engine(connect_param)
+
+    return engine
